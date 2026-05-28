4f577888f8c50dcae41f5467c9cfc16c581db87a
Diego <ds9053320@gmail.com>
2026-05-28 03:59:05 -0500

    Corregi el modelo de base de datos para tener compatibilidad con SQLAlchemy

--- app/models.py
+++ app/models.py
@@ -4,64 +4,20 @@ from datetime import datetime
 
 class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombreUsuario = db.Column('username', db.String(80), unique=True, nullable=False)
-    contrasenhaEncriptada = db.Column('password', db.String(200), nullable=False)
-
-    @property
-    def username(self):
-        return self.nombreUsuario
-
-    @property
-    def password(self):
-        return self.contrasenhaEncriptada
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 class Producto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombreProducto = db.Column('nombre', db.String(100), nullable=False)
-    precioProducto = db.Column('precio', db.Float, nullable=False)
-    stockDisponible = db.Column('stock', db.Integer, nullable=False, default=0)
-    categoriaProducto = db.Column('categoria', db.String(50))
-
-    @property
-    def nombre(self):
-        return self.nombreProducto
-
-    @property
-    def precio(self):
-        return self.precioProducto
-
-    @property
-    def stock(self):
-        return self.stockDisponible
-
-    @property
-    def categoria(self):
-        return self.categoriaProducto
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False, default=0)
+    categoria = db.Column(db.String(50))
 
 class Venta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    idProductoVendido = db.Column('producto_id', db.Integer, db.ForeignKey('producto.id'), nullable=False)
-    cantidadVendida = db.Column('cantidad', db.Integer, nullable=False)
-    totalVenta = db.Column('total', db.Float, nullable=False)
-    fechaVenta = db.Column('fecha', db.DateTime, default=datetime.utcnow)
-    productoRelacionado = db.relationship('Producto', backref='ventas')
-
-    @property
-    def producto_id(self):
-        return self.idProductoVendido
-
-    @property
-    def cantidad(self):
-        return self.cantidadVendida
-
-    @property
-    def total(self):
-        return self.totalVenta
-
-    @property
-    def fecha(self):
-        return self.fechaVenta
-
-    @property
-    def producto(self):
-        return self.productoRelacionado
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+    total = db.Column(db.Float, nullable=False)
+    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+    producto = db.relationship('Producto', backref='ventas')
